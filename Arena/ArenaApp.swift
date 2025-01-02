74f6e87f326117c39a1510962a7c1607278228d6
Aayush Kapoor <aayushkapoor34@gmail.com>
2025-01-02 14:23:21 +0530

    Initial Commit

---
+++ Arena/ArenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArenaApp.swift
+//  Arena
+//
+//  Created by Aayush Kapoor on 02/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ArenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
